858ba2f769cdc99f3442dc78285aa8fc99789e07
Aminulhoque01 <aminuhoque53@gmail.com>
2025-11-12 00:47:11 +0600

    productlisting page done

---
+++ src/components/ProductItemListView/ProductItemListView.jsx
@@ -0,0 +1,106 @@
+import { Link } from "react-router-dom";
+import "../ProductItems/style.css";
+import Rating from "@mui/material/Rating";
+import Button from "@mui/material/Button";
+import { FaRegHeart } from "react-icons/fa";
+import { LuGitCompareArrows } from "react-icons/lu";
+import { MdZoomOutMap } from "react-icons/md";
+import Tooltip from "@mui/material/Tooltip";
+import { IoMdCart } from "react-icons/io";
+import "./style.css"
+
+const ProductItemListView = () => {
+  return (
+    <div className="productItem shadow-lg rounded-md overflow-hidden border-1
+     border-[rgba(0,0,0,0.1)] flex items-center">
+      <div className="group imageWrapper w-[25%]  rounded-md relative">
+        <Link to="/">
+          <div className="img h-[220px] overflow-hidden">
+            <img
+              src="https://serviceapi.spicezgold.com/download/1742462909156_gdgd1.jpg"
+              alt=""
+              className="w-full rounded-md "
+            />
+            <img
+              src="https://serviceapi.spicezgold.com/download/1742462909158_gdgd2.jpg"
+              alt=""
+              className="w-full h-full transition-all duration-700 absolute top-0 left-0 opacity-0 group-hover:opacity-100 group-hover:scale-105"
+            />
+          </div>
+        </Link>
+        <span
+          className="discount flex items-center absolute top-[10px] left-[10px] z-50 bg-primary
+                text-white rounded-md p-1 text-[12px] font-[500]"
+        >
+          10%
+        </span>
+
+        <div
+          className="actions absolute top-[-200px] right-[5px] z-50 flex items-end gap-2 flex-col w-[50px]
+         transition-all duration-500 group-hover:top-[15px] opacity-0 group-hover:opacity-100"
+        >
+          <Tooltip title="favorite" placement="left-start">
+            {" "}
+            <Button
+              className="!w-[35px] !h-[35px] !min-w-[35px] !rounded-full !text-black !bg-white
+                     hover:!bg-primary  group"
+            >
+              <FaRegHeart className="text-[18px] !text-black hover:!text-white" />
+            </Button>
+          </Tooltip>
+          <Tooltip title="compare" placement="left-start">
+            <Button
+              className="!w-[35px] !h-[35px] !min-w-[35px] !rounded-full !text-black !bg-white
+                     hover:!bg-primary hover:!text-white group"
+            >
+              <LuGitCompareArrows className="text-[18px] !text-black hover:!text-white " />
+            </Button>
+          </Tooltip>
+          <Tooltip title="view details" placement="left-start">
+            <Button
+              className="!w-[35px] !h-[35px] !min-w-[35px] !rounded-full !text-black !bg-white
+                     hover:!bg-primary hover:!text-white group"
+            >
+              <MdZoomOutMap className="text-[18px] !text-black hover:!text-white " />
+            </Button>
+          </Tooltip>
+        </div>
+      </div>
+
+      <div className="info p-3 py-5 px-8  w-[75%]">
+        <h6 className="text-[15px]">
+          <Link to="/" className="link transition-all">
+            Soylent Green
+          </Link>
+        </h6>
+        <h3 className="text-[18px] title mt-2 mb-3 font-[500] text-[rgba(0,0,0,0.9)]">
+          <Link to="/" className="link transition-all">
+            Siril Georgette pink color Saree with Blouse piece
+          </Link>
+        </h3>
+        <p className="text-[14px] mb-3">We denounce with righteous indignation and dislike men who are so beguild and demoralized by the charms of pleasure of the moment, so blinded by desire that they cannot</p>
+        <Rating
+          name="half-rating-read"
+          defaultValue={2.5}
+          precision={0.5}
+          readOnly
+        />
+
+        <div className="flex items-center gap-4">
+          <span className="oldPrice line-through text-gray-500 text-[15px] fon-[500]">
+            $58.00
+          </span>
+          <span className="newPrice text-primary text-[15px] font-[600]">
+            $50.00
+          </span>
+        </div>
+
+        <div className="mt-3">
+          <Button className="btn-org flex gap-2"><IoMdCart className="text-[20px]"/>Ad to Cart</Button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ProductItemListView;
--- src/pages/ProductListing/ProductListing.jsx
+++ src/pages/ProductListing/ProductListing.jsx
@@ -6,11 +6,27 @@ import ProductItems from "../../components/ProductItems/ProductItems";
 import Button from "@mui/material/Button";
 import { IoGridSharp } from "react-icons/io5";
 import { GiHamburgerMenu } from "react-icons/gi";
-
+import Menu from "@mui/material/Menu";
+import MenuItem from "@mui/material/MenuItem";
+import { useState } from "react";
+import ProductItemListView from "../../components/ProductItemListView/ProductItemListView";
+import Pagination from '@mui/material/Pagination';
+import Stack from '@mui/material/Stack';
+import "./style.css"
 
 const ProductListing = () => {
+  const [itemView, setItemView]=useState("grid")
+  const [anchorEl, setAnchorEl] = useState(null);
+  const open = Boolean(anchorEl);
+  const handleClick = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
   return (
-    <section className="py-5">
+    <section className="py-5 pb-0">
       <div className="container">
         <Breadcrumbs aria-label="breadcrumb">
           <Link underline="hover" color="inherit" href="/" className="link">
@@ -28,31 +44,101 @@ const ProductListing = () => {
             <Sidebar />
           </div>
 
-          <div className="rightContent w-[80%]">
-            <div className="bg-[#f1f1f1] p-2 w-full mb-3 rounded-md flex items-center justify-between">
-                <div className="col1 flex items-center">
-                  <Button className="!w-[40px] !h-[40px] !min-w-[40px] !rounded-full
-                   !text-[#000]"><IoGridSharp className="text-[rgba(0,0,0,0.7)]"/></Button>
+          <div className="rightContent w-[80%] py-3">
+            <div className="bg-[#f1f1f1] p-2 w-full mb-4 rounded-md flex items-center justify-between">
+              <div className="col1 flex items-center itemViewActions">
+                <Button
+                  className={`!w-[40px] !h-[40px] !min-w-[40px] !rounded-full
+                   !text-[#000] ${itemView ==="list" && 'active'} action `}
+                   onClick={()=>setItemView('list')}
+                >
+                  <GiHamburgerMenu className="text-[rgba(0,0,0,0.7)]" />
+                </Button>
 
-                  <Button className="!w-[40px] !h-[40px] !min-w-[40px] !rounded-full
-                   !text-[#000]"><GiHamburgerMenu className="text-[rgba(0,0,0,0.7)]"/></Button>
-                </div>
-            </div>
-            <div className="grid grid-cols-4 md:grid-cols-4 gap-4">
-              <ProductItems/>
-              <ProductItems/>
-              <ProductItems/>
-              <ProductItems/>
-              <ProductItems/>
-              <ProductItems/>
-              <ProductItems/>
-              <ProductItems/>
-              <ProductItems/>
-              <ProductItems/>
-              <ProductItems/>
-              <ProductItems/>
-            </div>
+                <Button
+                  className={`!w-[40px] !h-[40px] !min-w-[40px] !rounded-full
+                   !text-[#000] ${itemView === "grid" && 'active'}`}
+                   onClick={()=>setItemView('grid')}
+                >
+                  <IoGridSharp className="text-[rgba(0,0,0,0.7)]" />
+                </Button>
 
+                <span className="text-[14px] font-[500] pl-3 text-[rgba(0,0,0,0.7)]">
+                  There are 27 products
+                </span>
+              </div>
+
+              <div className="col2 ml-auto flex items-center justify-end gap-3">
+                <span className="text-[14px] font-[500] pl-3 text-[rgba(0,0,0,0.7)]">
+                  Sort By
+                </span>
+                <Button
+                  id="basic-button"
+                  aria-controls={open ? "basic-menu" : undefined}
+                  aria-haspopup="true"
+                  aria-expanded={open ? "true" : undefined}
+                  onClick={handleClick}
+                  className="!bg-white !text-[12px] !text-[#000] !capitalize !border-2 !border-[#000]"
+                >
+                  Dashboard
+                </Button>
+                <Menu
+                  id="basic-menu"
+                  anchorEl={anchorEl}
+                  open={open}
+                  onClose={handleClose}
+                  slotProps={{
+                    list: {
+                      "aria-labelledby": "basic-button",
+                    },
+                  }}
+                >
+                  <MenuItem onClick={handleClose} className="!text-[13px] !text-[#000] !capitalize">Sales,height to lowest</MenuItem>
+                  <MenuItem onClick={handleClose} className="!text-[13px] !text-[#000] !capitalize">Relevance</MenuItem>
+                  <MenuItem onClick={handleClose} className="!text-[13px] !text-[#000] !capitalize">Name, A to Z</MenuItem>
+                  <MenuItem onClick={handleClose} className="!text-[13px] !text-[#000] !capitalize">Name, Z to A</MenuItem>
+                  <MenuItem onClick={handleClose} className="!text-[13px] !text-[#000] !capitalize">Price,low to high</MenuItem>
+                  <MenuItem onClick={handleClose} className="!text-[13px] !text-[#000] !capitalize">Price,high to low</MenuItem>
+                </Menu>
+              </div>
+            </div>
+            <div className={`grid ${itemView === "grid" ? "grid-cols-4  md:grid-cols-4" : "grid-cols-1  md:grid-cols-1"}  gap-4`}>
+
+             {
+               itemView === 'grid' ?
+
+               <>
+               <ProductItems />
+              <ProductItems />
+              <ProductItems />
+              <ProductItems />
+              <ProductItems />
+              <ProductItems />
+              <ProductItems />
+              <ProductItems />
+              <ProductItems />
+              <ProductItems />
+              <ProductItems />
+              <ProductItems />
+               </>
+               :
+
+               <>
+                <ProductItemListView/>
+                <ProductItemListView/>
+                <ProductItemListView/>
+                <ProductItemListView/>
+                <ProductItemListView/>
+                <ProductItemListView/>
+                <ProductItemListView/>
+                <ProductItemListView/>
+               </>
+             }
+              
+            </div>
+             <div className="flex items-center justify-center mt-10 pagination">
+              <Pagination count={10} showFirstButton showLastButton />
+             </div>
           </div>
         </div>
       </div>
