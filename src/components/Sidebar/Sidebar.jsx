951f22bc50fd13de6e0bac4dbbd14abc31e6e7ac
Aminulhoque01 <aminuhoque53@gmail.com>
2025-11-11 19:28:46 +0600

    productlisiting

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -9,10 +9,14 @@ import { IoIosArrowUp } from "react-icons/io";
 import "./style.css";
 import Button from "@mui/material/Button";
 import { useState } from "react";
+import RangeSlider from 'react-range-slider-input';
+import 'react-range-slider-input/dist/style.css';
+import Rating from '@mui/material/Rating';
 
 const Sidebar = () => {
   const [isOpenCategoryFilter, setIsOpenCategoryFilter] = useState(true);
-  const [isOpenAvailabilityFilter, setIsOpenAvailabilityFilter] = useState(true);
+  const [isOpenAvailabilityFilter, setIsOpenAvailabilityFilter] =
+    useState(true);
   const [isOpenSizeFilter, setIsSizeFilter] = useState(true);
 
   return (
@@ -25,7 +29,7 @@ const Sidebar = () => {
             onClick={() => setIsOpenCategoryFilter(!isOpenCategoryFilter)}
           >
             {isOpenCategoryFilter === true ? (
-              <IoIosArrowUp className="text-[#000] !text-[35px] pt-1  "/>
+              <IoIosArrowUp className="text-[#000] !text-[35px] pt-1  " />
             ) : (
               <IoIosArrowDown className="text-[#000] !text-[35px] pt-1  " />
             )}
@@ -49,13 +53,15 @@ const Sidebar = () => {
 
       <div className="box mb-4 ">
         <h3 className="mb-3 text-[16px] font-[600] flex items-center pr-5 ">
-         Availability
+          Availability
           <Button
             className="!w-[30px] !h-[30px] !min-w-[30px] !rounded-full !ml-auto"
-            onClick={() => setIsOpenAvailabilityFilter(!isOpenAvailabilityFilter)}
+            onClick={() =>
+              setIsOpenAvailabilityFilter(!isOpenAvailabilityFilter)
+            }
           >
             {isOpenAvailabilityFilter === true ? (
-              <IoIosArrowUp className="text-[#000] !text-[35px] pt-1  "/>
+              <IoIosArrowUp className="text-[#000] !text-[35px] pt-1  " />
             ) : (
               <IoIosArrowDown className="text-[#000] !text-[35px] pt-1  " />
             )}
@@ -67,7 +73,6 @@ const Sidebar = () => {
               <FormControlLabel control={<Checkbox />} label="Available (17)" />
               <FormControlLabel control={<Checkbox />} label="In Stock  (17)" />
               <FormControlLabel control={<Checkbox />} label="Not Available" />
-              
             </FormGroup>
           </div>
         </UnmountClosed>
@@ -75,13 +80,13 @@ const Sidebar = () => {
 
       <div className="box  ">
         <h3 className="mb-3 text-[16px] font-[600] flex items-center pr-5 ">
-         Size
+          Size
           <Button
             className="!w-[30px] !h-[30px] !min-w-[30px] !rounded-full !ml-auto"
             onClick={() => setIsSizeFilter(!isOpenSizeFilter)}
           >
             {isOpenSizeFilter === true ? (
-              <IoIosArrowUp className="text-[#000] !text-[35px] pt-1  "/>
+              <IoIosArrowUp className="text-[#000] !text-[35px] pt-1  " />
             ) : (
               <IoIosArrowDown className="text-[#000] !text-[35px] pt-1  " />
             )}
@@ -95,11 +100,51 @@ const Sidebar = () => {
               <FormControlLabel control={<Checkbox />} label="Large (10)" />
               <FormControlLabel control={<Checkbox />} label="Xl (15)" />
               <FormControlLabel control={<Checkbox />} label="XXl (25)" />
-              
             </FormGroup>
           </div>
         </UnmountClosed>
       </div>
+
+       <div className="box  mt-4">
+            <h3 className="mb-3 text-[16px] font-[600] flex items-center pr-5 ">
+            Filter By Price
+            </h3>
+
+            <RangeSlider/>
+
+            <div className="flex pt-4 pb-2 priceRange ">
+                <span className="  text-[13px]">
+                    From:<strong className="text-dark">Rs:{100}</strong>
+                </span>
+                <span className="ml-auto text-[13px]">
+                    From:<strong className="text-dark">Rs:{500}</strong>
+                </span>
+            </div>
+       </div>
+
+       <div className="box  mt-4">
+            <h3 className="mb-3 text-[16px] font-[600] flex items-center pr-5 ">
+            Filter by Rating
+            </h3>
+
+           <div className="w-full">
+             <Rating name="size-small" defaultValue={5} size="small"   />
+           </div>
+           <div className="w-full">
+             <Rating name="size-small" defaultValue={4} size="small"   />
+           </div>
+           <div className="w-full">
+             <Rating name="size-small" defaultValue={3} size="small"   />
+           </div>
+           <div className="w-full">
+             <Rating name="size-small" defaultValue={2} size="small"   />
+           </div>
+           <div className="w-full">
+             <Rating name="size-small" defaultValue={1} size="small"   />
+           </div>
+             
+            
+       </div>
     </aside>
   );
 };
--- src/pages/ProductListing/ProductListing.jsx
+++ src/pages/ProductListing/ProductListing.jsx
@@ -2,6 +2,8 @@ import Sidebar from "../../components/Sidebar/Sidebar";
 import Typography from "@mui/material/Typography";
 import Breadcrumbs from "@mui/material/Breadcrumbs";
 import Link from "@mui/material/Link";
+import ProductItems from "../../components/ProductItems/ProductItems";
+import Button from "@mui/material/Button";
 
 const ProductListing = () => {
   return (
@@ -11,24 +13,41 @@ const ProductListing = () => {
           <Link underline="hover" color="inherit" href="/" className="link">
             Home
           </Link>
-          <Link
-            underline="hover"
-            color="inherit"
-            href="/"
-            className="link"
-          >
+          <Link underline="hover" color="inherit" href="/" className="link">
             Fashion
           </Link>
-         
         </Breadcrumbs>
       </div>
 
       <div className="bg-white p-3 mt-4">
-        <div className="container flex gap-3">
-        <div className="sidebarWrapper w-[20%] h-[100%] bg-white  ">
-          <Sidebar />
+        <div className="container flex gap-5">
+          <div className="sidebarWrapper w-[20%] h-[100%] bg-white  ">
+            <Sidebar />
+          </div>
+
+          <div className="rightContent w-[80%]">
+            <div className="bg-[#f1f1f1] p-2 w-full mb-3 rounded-md flex items-center justify-between">
+                <div className="col1 flex items-center gap-3">
+                  <Button></Button>
+                </div>
+            </div>
+            <div className="grid grid-cols-4 md:grid-cols-4 gap-4">
+              <ProductItems/>
+              <ProductItems/>
+              <ProductItems/>
+              <ProductItems/>
+              <ProductItems/>
+              <ProductItems/>
+              <ProductItems/>
+              <ProductItems/>
+              <ProductItems/>
+              <ProductItems/>
+              <ProductItems/>
+              <ProductItems/>
+            </div>
+
+          </div>
         </div>
-      </div>
       </div>
     </section>
   );
