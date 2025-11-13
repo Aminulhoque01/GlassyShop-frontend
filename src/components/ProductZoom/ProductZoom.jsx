8bfb5e5cbc84cd232c3456e899297947adb80445
Aminulhoque01 <aminuhoque53@gmail.com>
2025-11-13 23:17:45 +0600

    productzoom

--- src/components/ProductZoom/ProductZoom.jsx
+++ src/components/ProductZoom/ProductZoom.jsx
@@ -1,11 +1,61 @@
+import InnerImageZoom from "react-inner-image-zoom";
+import "react-inner-image-zoom/lib/styles.min.css";
+import shirt from "../../assets/product/shart1.jpg";
+import shirt2 from "../../assets/product/shart2.jpg";
+import { Swiper, SwiperSlide } from "swiper/react";
 
+// Import Swiper styles
+import "swiper/css";
+import "swiper/css/effect-fade";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
 
-const ProductZoom=()=>{
-    return(
-        <div>
+import { Navigation, Pagination } from "swiper/modules";
 
+const ProductZoom = () => {
+  return (
+    <>
+      <div className="flex gap-4">
+        <div className="slider w-[30%]">
+          <Swiper
+            direction={"vertical"}
+            slidesPerView={4}
+            spaceBetween={10}
+            navigation={true}
+            pagination={{
+              clickable: true,
+            }}
+            modules={[Navigation, Pagination]}
+            className="zoomProductSliderThumbs"
+          >
+            <SwiperSlide>
+              <div className="item">
+                <img src={shirt2} alt="" />
+              </div>
+            </SwiperSlide>
+            <SwiperSlide>
+              <div className="item">
+                <img src={shirt2} alt="" />
+              </div>
+            </SwiperSlide>
+            <SwiperSlide>
+              <div className="item">
+                <img src={shirt2} alt="" />
+              </div>
+            </SwiperSlide>
+            <SwiperSlide>
+              <div className="item">
+                <img src={shirt2} alt="" />
+              </div>
+            </SwiperSlide>
+          </Swiper>
         </div>
-    )
+        <div className="zoomContainer w-[70%]">
+          <InnerImageZoom src={shirt} zoomType="hover" zoomScale={1} />
+        </div>
+      </div>
+    </>
+  );
 };
 
-export default ProductZoom
+export default ProductZoom;
--- src/pages/ProductDetails/ProductDetails.jsx
+++ src/pages/ProductDetails/ProductDetails.jsx
@@ -4,25 +4,28 @@ import ProductZoom from "../../components/ProductZoom/ProductZoom";
 
 const ProductDetails = () => {
   return (
-    <section className="py-5 pb-0">
-      <div className="container">
-        <Breadcrumbs aria-label="breadcrumb">
-          <Link underline="hover" color="inherit" href="/" className="link">
-            Home
-          </Link>
-          <Link underline="hover" color="inherit" href="/" className="link">
-            Fashion
-          </Link>
-        </Breadcrumbs>
-      </div>
-
-      <div className="container flex gap-4">
-        <div className="productZoomContainer">
-            <ProductZoom/>
+    <>
+      <div className="py-5 pb-0">
+        <div className="container">
+          <Breadcrumbs aria-label="breadcrumb">
+            <Link underline="hover" color="inherit" href="/" className="link">
+              Home
+            </Link>
+            <Link underline="hover" color="inherit" href="/" className="link">
+              Fashion
+            </Link>
+          </Breadcrumbs>
         </div>
-
       </div>
-    </section>
+
+      <section className="bg-white py-5">
+        <div className="container flex gap-4">
+          <div className="productZoomContainer w-[30%]">
+            <ProductZoom />
+          </div>
+        </div>
+      </section>
+    </>
   );
 };
 
