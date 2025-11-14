1765e9c8141507f786d9767ac121ac37635d7ad8
Aminulhoque01 <aminuhoque53@gmail.com>
2025-11-14 21:52:32 +0600

    slider

--- src/components/ProductZoom/ProductZoom.jsx
+++ src/components/ProductZoom/ProductZoom.jsx
@@ -2,11 +2,14 @@ import InnerImageZoom from "react-inner-image-zoom";
 import "react-inner-image-zoom/lib/styles.min.css";
 import shirt from "../../assets/product/shart1.jpg";
 import shirt2 from "../../assets/product/shart2.jpg";
+import shirt3 from "../../assets/product/shirt3.jpg";
+import shirt4 from "../../assets/product/shirt4.jpg";
+
 import { Swiper, SwiperSlide } from "swiper/react";
 
 // Import Swiper styles
-import 'swiper/css';
-import 'swiper/css/pagination';
+import "swiper/css";
+import "swiper/css/pagination";
 
 import { Navigation, Pagination } from "swiper/modules";
 import "./style.css";
@@ -21,49 +24,95 @@ const ProductZoom = () => {
             slidesPerView={4}
             spaceBetween={10}
             navigation={true}
-             
             modules={[Navigation]}
-            className="zoomProductSliderThumbs"
+            className="zoomProductSliderThumbs height-[100px] overflow-hidden"
           >
             <SwiperSlide>
-              <div className="item">
-                <img src={shirt2} alt="" />
+              <div className="item rounded-md overflow-hidden cursor-pointer group">
+                <img
+                  src={shirt2}
+                  alt=""
+                  className="w-full transition-all group-hover:scale-105"
+                />
               </div>
             </SwiperSlide>
             <SwiperSlide>
-              <div className="item">
-                <img src={shirt2} alt="" />
+              <div className="item rounded-md overflow-hidden cursor-pointer group">
+                <img
+                  src={shirt2}
+                  alt=""
+                  className="w-full transition-all group-hover:scale-105"
+                />
               </div>
             </SwiperSlide>
             <SwiperSlide>
-              <div className="item">
-                <img src={shirt2} alt="" />
+              <div className="item rounded-md overflow-hidden cursor-pointer group">
+                <img
+                  src={shirt3}
+                  alt=""
+                  className="w-full transition-all group-hover:scale-105"
+                />
               </div>
             </SwiperSlide>
             <SwiperSlide>
-              <div className="item">
-                <img src={shirt2} alt="" />
+              <div className="item rounded-md overflow-hidden cursor-pointer group">
+                <img
+                  src={shirt4}
+                  alt=""
+                  className="w-full transition-all group-hover:scale-105"
+                />
               </div>
             </SwiperSlide>
             <SwiperSlide>
-              <div className="item">
-                <img src={shirt2} alt="" />
+              <div className="item rounded-md overflow-hidden cursor-pointer group">
+                <img
+                  src={shirt2}
+                  alt=""
+                  className="w-full transition-all group-hover:scale-105"
+                />
               </div>
             </SwiperSlide>
             <SwiperSlide>
-              <div className="item">
-                <img src={shirt2} alt="" />
+              <div className="item rounded-md overflow-hidden cursor-pointer group">
+                <img
+                  src={shirt2}
+                  alt=""
+                  className="w-full transition-all group-hover:scale-105"
+                />
               </div>
             </SwiperSlide>
             <SwiperSlide>
-              <div className="item">
-                <img src={shirt2} alt="" />
+              <div className="item rounded-md overflow-hidden cursor-pointer group">
+                <img
+                  src={shirt2}
+                  alt=""
+                  className="w-full transition-all group-hover:scale-105"
+                />
               </div>
             </SwiperSlide>
           </Swiper>
         </div>
-        <div className="zoomContainer w-[80%] h-[70vh] overflow-hidden">
-          <InnerImageZoom src={shirt} zoomType="hover" zoomScale={1} />
+        <div className="zoomContainer w-[80%]   overflow-hidden">
+          <Swiper
+            slidesPerView={1}
+            spaceBetween={0}
+            navigation={false}
+            
+          >
+            <SwiperSlide>
+              <InnerImageZoom src={shirt} zoomType="hover" zoomScale={1} />
+            </SwiperSlide>
+             
+            <SwiperSlide>
+              <InnerImageZoom src={shirt2} zoomType="hover" zoomScale={1} />
+            </SwiperSlide>
+            <SwiperSlide>
+              <InnerImageZoom src={shirt3} zoomType="hover" zoomScale={1} />
+            </SwiperSlide>
+            <SwiperSlide>
+              <InnerImageZoom src={shirt4} zoomType="hover" zoomScale={1} />
+            </SwiperSlide>
+          </Swiper>
         </div>
       </div>
     </>
