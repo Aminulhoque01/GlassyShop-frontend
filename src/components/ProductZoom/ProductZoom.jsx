86760d9f43cb2659e1aa4760ca5863603fd29507
Aminulhoque01 <aminuhoque53@gmail.com>
2025-11-14 12:10:12 +0600

    ok

--- src/components/ProductZoom/ProductZoom.jsx
+++ src/components/ProductZoom/ProductZoom.jsx
@@ -5,27 +5,24 @@ import shirt2 from "../../assets/product/shart2.jpg";
 import { Swiper, SwiperSlide } from "swiper/react";
 
 // Import Swiper styles
-import "swiper/css";
-import "swiper/css/effect-fade";
-import "swiper/css/navigation";
-import "swiper/css/pagination";
+import 'swiper/css';
+import 'swiper/css/pagination';
 
 import { Navigation, Pagination } from "swiper/modules";
+import "./style.css";
 
 const ProductZoom = () => {
   return (
     <>
       <div className="flex gap-4">
-        <div className="slider w-[30%]">
+        <div className="slider w-[20%]">
           <Swiper
             direction={"vertical"}
             slidesPerView={4}
             spaceBetween={10}
             navigation={true}
-            pagination={{
-              clickable: true,
-            }}
-            modules={[Navigation, Pagination]}
+             
+            modules={[Navigation]}
             className="zoomProductSliderThumbs"
           >
             <SwiperSlide>
@@ -48,9 +45,24 @@ const ProductZoom = () => {
                 <img src={shirt2} alt="" />
               </div>
             </SwiperSlide>
+            <SwiperSlide>
+              <div className="item">
+                <img src={shirt2} alt="" />
+              </div>
+            </SwiperSlide>
+            <SwiperSlide>
+              <div className="item">
+                <img src={shirt2} alt="" />
+              </div>
+            </SwiperSlide>
+            <SwiperSlide>
+              <div className="item">
+                <img src={shirt2} alt="" />
+              </div>
+            </SwiperSlide>
           </Swiper>
         </div>
-        <div className="zoomContainer w-[70%]">
+        <div className="zoomContainer w-[80%] h-[70vh] overflow-hidden">
           <InnerImageZoom src={shirt} zoomType="hover" zoomScale={1} />
         </div>
       </div>
