8d1541ec367af8c3f14b9155ac1fc29aebe6b237
Aminulhoque01 <aminuhoque53@gmail.com>
2025-11-19 23:22:58 +0600

    add modal

--- src/App.jsx
+++ src/App.jsx
@@ -5,18 +5,71 @@ import ProductListing from "./pages/ProductListing/ProductListing";
 import Footer from "./components/Footer/Footer";
 import ProductDetails from "./pages/ProductDetails/ProductDetails";
 
+import Button from "@mui/material/Button";
+import Dialog from "@mui/material/Dialog";
+import DialogActions from "@mui/material/DialogActions";
+import DialogContent from "@mui/material/DialogContent";
+import DialogContentText from "@mui/material/DialogContentText";
+import DialogTitle from "@mui/material/DialogTitle";
+import { createContext, useState } from "react";
+import ProductZoom from "./components/ProductZoom/ProductZoom";
+
+const MyContext = createContext();
+
 function App() {
+  const [openProductDetailsModal, setOpenProductDetailsModal] = useState(true);
+  const [fullWidth, setFullWidth] = useState(true);
+  const [maxWidth, setMaxWidth] = useState("md");
+
+  const handleClickOpenProductDetailsModal = () => {
+    setOpenProductDetailsModal(true);
+  };
+
+  const handleCloseProductDetailsModal = () => {
+    setOpenProductDetailsModal(false);
+  };
+
+  const values = {};
+
   return (
     <>
       <BrowserRouter>
-        <Header />
-        <Routes>
-          <Route path={"/"} exact={true} element={<Home/>}/>
-          <Route path={"/productListing"} exact={true} element={<ProductListing/>}/>
-          <Route path={"/productDetails/:id"} exact={true} element={<ProductDetails/>}/>
-        </Routes>
-        <Footer />
+        <MyContext.Provider value={values}>
+          <Header />
+          <Routes>
+            <Route path={"/"} exact={true} element={<Home />} />
+            <Route
+              path={"/productListing"}
+              exact={true}
+              element={<ProductListing />}
+            />
+            <Route
+              path={"/productDetails/:id"}
+              exact={true}
+              element={<ProductDetails />}
+            />
+          </Routes>
+          <Footer />
+        </MyContext.Provider>
       </BrowserRouter>
+
+      <Dialog
+        open={openProductDetailsModal}
+        onClose={handleCloseProductDetailsModal}
+        fullWidth={fullWidth}
+        maxWidth={maxWidth}
+        aria-labelledby="alert-dialog-title"
+        aria-describedby="alert-dialog-description"
+        className="productDetailsModal"
+      >
+        <DialogContent>
+          <div className="flex items-center w-full productDetailsModalContainer">
+            <div className="col1 w-[50%]">
+              <ProductZoom />
+            </div>
+          </div>
+        </DialogContent>
+      </Dialog>
     </>
   );
 }
--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: "192.168.0.101",
+    port: 5173,
+  }
  
 })
