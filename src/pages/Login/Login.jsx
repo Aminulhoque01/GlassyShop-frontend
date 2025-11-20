d10fcf75d40142da4a15aa8fb90fdf0fa181f3d2
Aminulhoque01 <aminuhoque53@gmail.com>
2025-11-20 23:44:05 +0600

    login page done

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -3,13 +3,16 @@ import TextField from "@mui/material/TextField";
 import { useState } from "react";
 import { IoEyeOutline } from "react-icons/io5";
 import { IoEyeOffOutline } from "react-icons/io5";
+import { Link } from "react-router-dom";
+import { FcGoogle } from "react-icons/fc";
+
 
 const Login = () => {
     const [isShowPassword,setIsShowPassword]=useState(false)
   return (
     <section className="section py-10">
       <div className="container">
-        <div className="card shadow-md w-[500px] m-auto rounded-md bg-white p-5 px-12">
+        <div className="card shadow-md w-[400px] m-auto rounded-md bg-white p-5 px-12">
           <h1 className="text-center text-[18px] font-[600] text-black">
             Login to your account
           </h1>
@@ -40,7 +43,19 @@ const Login = () => {
                   }
                 </Button>
               </div>
+
+              <a href="" className="link cursor-pointer text-[14px] font-[500]">Forget Password ?</a>
             </div>
+
+            <div className="flex items-center w-full mt-3 mb-3">
+                <Button className="btn-org btn-lg w-full">Login</Button>
+
+            </div>
+
+            <p className="text-center">Not Registered? <Link className="link text-[14px] font-[600] text-primary" to="/register">Sign Up</Link></p>
+
+            <p className="text-center mt-3 font-[500] mb-3">Or continue with social account</p>
+            <Button className="flex gap-3  w-full !bg-[#f1f1f1] btn-lg !text-black"><FcGoogle className="text-[22px]"/>SignIn With Google</Button>
           </form>
         </div>
       </div>
