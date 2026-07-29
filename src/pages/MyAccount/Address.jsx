c063386b5eed30df44232794f14ce4c63fbc22f5
Aminulhoque01 <aminuhoque53@gmail.com>
2026-07-29 13:26:42 +0600

    adress

--- src/pages/MyAccount/Address.jsx
+++ src/pages/MyAccount/Address.jsx
@@ -1,8 +1,32 @@
+import AccountSidebar from "../../components/AccountSidebar/AccountSidebar";
 
 
 const Address=()=>{
     return(
-        <div>address</div>
+        <div>
+            <section className="py-10 w-full">
+                  <div className="container flex gap-5">
+                    <div className="col1 w-[20%]">
+                      <AccountSidebar />
+                    </div>
+            
+                    <div className="col2 w-[50%]">
+                      <div className="card bg-white p-5 shadow-md rounded-md mb-5">
+                        <div className="flex items-center pb-0">
+                          <h2 className="pb-0">Address</h2>
+                           
+                        </div>
+                        <hr />
+            
+                        
+                      </div>
+            
+                     
+                       
+                    </div>
+                  </div>
+                </section>
+        </div>
     )
 }
 
